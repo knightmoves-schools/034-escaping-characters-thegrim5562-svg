4a923d180dae14e475217c8742249d4f41ec2cdf
raishaun cooke <thegrim5562@gmail.com>
2026-05-05 10:20:48 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let introduction = 'my name,\n' +
+  "\iIs raishaun and i am \'smart\' aswell.\n" 
+'I am \"passing"\ all my classes. \n' +
+
+  document.getElementById("result").innderHTML = introduction;
