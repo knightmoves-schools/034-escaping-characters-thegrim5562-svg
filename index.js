4c7ccc1e7d7f3bca307d9eb26a0d94722a2c7268
raishaun cooke <thegrim5562@gmail.com>
2026-05-05 10:27:11 -0600

    Fix string concatenation and HTML property typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let introduction = 'my name,\n' +
-'\iIs raishaun and i am \'smart\' aswell.\n'
+'\iIs raishaun and i am \'smart\' aswell.\n' +
 "I am \"passing"\ all my classes. \n" +
 
-  document.getElementById("result").innderHTML = introduction;
+document.getElementById("result").innderHTML = introduction;
